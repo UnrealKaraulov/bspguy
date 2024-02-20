785117291a4b1ae431dd9982d49e06ff657a2850
Unreal Karaulov <karaul0v@list.ru>
2024-02-20 12:51:28 +0300

    newbspguy version increase number value

--- src/main.cpp
+++ src/main.cpp
@@ -98,7 +98,7 @@
 // Removing HULL 0 from solid model crashes game when standing on it
 
 
-std::string g_version_string = "NewBSPGuy v4.15";
+std::string g_version_string = "NewBSPGuy v4.16";
 
 bool g_verbose = false;
 
