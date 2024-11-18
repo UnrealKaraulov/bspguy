62fda804da92f41b6b168c5d33ae69a210a9e860
Unreal Karaulov <karaul0v@list.ru>
2024-11-18 17:48:22 +0300

    Update win32_platform.h

--- glfw/src/win32_platform.h
+++ glfw/src/win32_platform.h
@@ -68,9 +68,95 @@
 #endif
 
 // GLFW uses OEM cursor resources
+
+#ifdef OEMRESOURCE
+
+
+
 #ifndef OEMRESOURCE
 #define OEMRESOURCE
 #endif
+/*
+ * OEM Resource Ordinal Numbers
+ */
+#define OBM_CLOSE           32754
+#define OBM_UPARROW         32753
+#define OBM_DNARROW         32752
+#define OBM_RGARROW         32751
+#define OBM_LFARROW         32750
+#define OBM_REDUCE          32749
+#define OBM_ZOOM            32748
+#define OBM_RESTORE         32747
+#define OBM_REDUCED         32746
+#define OBM_ZOOMD           32745
+#define OBM_RESTORED        32744
+#define OBM_UPARROWD        32743
+#define OBM_DNARROWD        32742
+#define OBM_RGARROWD        32741
+#define OBM_LFARROWD        32740
+#define OBM_MNARROW         32739
+#define OBM_COMBO           32738
+#define OBM_UPARROWI        32737
+#define OBM_DNARROWI        32736
+#define OBM_RGARROWI        32735
+#define OBM_LFARROWI        32734
+
+#define OBM_OLD_CLOSE       32767
+#define OBM_SIZE            32766
+#define OBM_OLD_UPARROW     32765
+#define OBM_OLD_DNARROW     32764
+#define OBM_OLD_RGARROW     32763
+#define OBM_OLD_LFARROW     32762
+#define OBM_BTSIZE          32761
+#define OBM_CHECK           32760
+#define OBM_CHECKBOXES      32759
+#define OBM_BTNCORNERS      32758
+#define OBM_OLD_REDUCE      32757
+#define OBM_OLD_ZOOM        32756
+#define OBM_OLD_RESTORE     32755
+
+
+#define OCR_NORMAL          32512
+#define OCR_IBEAM           32513
+#define OCR_WAIT            32514
+#define OCR_CROSS           32515
+#define OCR_UP              32516
+#define OCR_SIZE            32640   /* OBSOLETE: use OCR_SIZEALL */
+#define OCR_ICON            32641   /* OBSOLETE: use OCR_NORMAL */
+#define OCR_SIZENWSE        32642
+#define OCR_SIZENESW        32643
+#define OCR_SIZEWE          32644
+#define OCR_SIZENS          32645
+#define OCR_SIZEALL         32646
+#define OCR_ICOCUR          32647   /* OBSOLETE: use OIC_WINLOGO */
+#define OCR_NO              32648
+#if(WINVER >= 0x0500)
+#define OCR_HAND            32649
+#endif /* WINVER >= 0x0500 */
+#if(WINVER >= 0x0400)
+#define OCR_APPSTARTING     32650
+#endif /* WINVER >= 0x0400 */
+
+
+#define OIC_SAMPLE          32512
+#define OIC_HAND            32513
+#define OIC_QUES            32514
+#define OIC_BANG            32515
+#define OIC_NOTE            32516
+#if(WINVER >= 0x0400)
+#define OIC_WINLOGO         32517
+#define OIC_WARNING         OIC_BANG
+#define OIC_ERROR           OIC_HAND
+#define OIC_INFORMATION     OIC_NOTE
+#endif /* WINVER >= 0x0400 */
+#if(WINVER >= 0x0600)
+#define OIC_SHIELD          32518
+#endif /* WINVER >= 0x0600 */
+
+
+
+#endif /* OEMRESOURCE */
+
 
 #include <wctype.h>
 #include <windows.h>
